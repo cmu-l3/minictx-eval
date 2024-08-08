cde517b7d60ec6bff8f2ed778315be7e0ce07edd
Gudora <jiewenh@cs.cmu.edu>
2024-08-07 21:00:24 -0400

    remove comments

--- repl_wrapper.py
+++ repl_wrapper.py
@@ -30,7 +30,6 @@ class InteractiveThread(threading.Thread):
                 self.send_cmd(initialize_check)
             self.session.expect('"env": 0}\r\n\r\n', timeout=60)  #If context contains sorries, it will have more keys other than env
             self.init_complete.set()
-            # print(f"Session {self.session_id}: Successfully start repl and waiting for input.")
         except:
             self.init_complete.set()
             print(f"Session {self.session_id}: fail to initialize lean repl")
@@ -73,7 +72,6 @@ class InteractiveThread(threading.Thread):
                 self.session.expect(['\r\n\r\n', pexpect.EOF], timeout=10)
                 output = self.session.before.strip()
                 output_dict = json.loads(output)
-                # print("Received:", output_dict)
 
                 self.response = output_dict
                 self.cmd_response_condition.set()  
@@ -121,33 +119,4 @@ class InteractiveThread(threading.Thread):
         self.cmd_query_condition.set() 
         self.cmd_response_condition.set()  
         self.timer.cancel()
-
-
-if __name__ == "__main__":
-    repl_path = './repl'
-    lean_env_path = './PrimeNumberTheoremAnd'
-    
-    test = "import Mathlib.Analysis.Complex.CauchyIntegral\nimport Mathlib.Analysis.Complex.Convex\n\nopen Complex Set Topology\n\nopen scoped Interval\n\nvariable {z w : ℂ} {c : ℝ}\n\n/-%%\nThis files gathers definitions and basic properties about rectangles.\n%%-/\n\n/-%%\n\\begin{definition}\\label{Rectangle}\\lean{Rectangle}\\leanok\nA Rectangle has corners $z$ and $w \\in \\C$.\n\\end{definition}\n%%-/\n/- A `Rectangle` has corners `z` and `w`. -/\ndef Rectangle (z w : ℂ) : Set ℂ := [[z.re, w.re]] ×ℂ [[z.im, w.im]]\n\nnamespace Rectangle\n\n"
-
-    thread = InteractiveThread(1, repl_path, lean_env_path, initial_context = test)
-    thread.start()
-
-    # output = thread.submit_and_receive(cmd)
-    # print(output)
-
-    test = '''lemma symm : Rectangle z w = Rectangle w z := by sorry'''
-    
-    output = thread.submit_and_receive({"cmd": test, "env": 0})
-    print(output)
-
-    # output = thread.submit_and_receive({"cmd": "theorem amc12_2000_p11 (a b : ℝ) (h₀ : a ≠ 0 ∧ b ≠ 0) (h₁ : a * b = a - b) :\n    a / b + b / a - a * b = 2 := by sorry", "env": 1})
-    # print(output)
-    # output = thread.submit_and_receive({"tactic": "field_simp [h₀.1, h₀.2]", "proofState": 0})
-    # print(output)
-    # thread.submit_and_receive({"tactic": "simp only [h₁, mul_comm, mul_sub]", "proofState": 1})
-    # thread.submit_and_receive({"tactic": "ring", "proofState": 2})
-    # print("Output:", output)
-    thread.stop()
-    thread.join()
-
 
